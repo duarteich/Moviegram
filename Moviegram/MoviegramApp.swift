a8bbcfe7b9d8c991dfcdbd181eadea9faf87328a
Christyan Duarte <duarteich@gmail.com>
2024-07-23 23:55:41 -0600

    Initial Commit

---
+++ Moviegram/MoviegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviegramApp.swift
+//  Moviegram
+//
+//  Created by Christyan Duarte on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
